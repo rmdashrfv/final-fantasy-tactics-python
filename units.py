cbf07a7f0f44b0d4c62c361d6661f976b3705a71
Michael Law <michaellaw@gmx.com>
2018-10-27 19:44:52 -0400

    minor inheritance issue. Working on changing job and stats

--- units.py
+++ units.py
@@ -17,5 +17,39 @@ class Unit():
             'shoes': None
         }
 
-    def attack(target):
-        print(f'{self.name} attacked the target!')
+    def __repr__(self):
+        return f'{self.name} >> {self.job} ({self.hp})HP'
+
+    def change_job(self, new_job):
+        old_job = str(self.job)
+        self.job = str(new_job).upper()
+        print(f'{self.name} changed jobs: {old_job} ---> {self.job}')
+
+    def attack(self, target):
+        dmg = self.strength - target.defense
+        target.hp - dmg
+        print(f'{self.name} attacked {target.name}!')
+        print(f'{target.name} lost {dmg} HP')
+        if target.hp <= 0:
+            target.status = 'felled'
+            print(f'{target.name} has fallen!')
+        elif target.hp < target.hp / 2:
+            target.status = 'critical'
+            print(f'{target.name} is in critical condition!')
+
+
+class Chemist(Unit):
+    def __init__(self):
+        super().hp -= 25
+        self.strength -= 3
+        self.defense += 10
+
+    def __repr__(self):
+        return(f'{self}')
+
+
+unit1 = Unit('Ramza')
+unit2 = Unit('Delita')
+
+unit1.attack(unit2)
+unit1.change_job(Chemist())
