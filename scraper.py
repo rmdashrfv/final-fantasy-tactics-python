c349a88838638d40933151814a296627d507612b
Michael Law 2 <flyinggeese@gmx.com>
2018-11-15 16:28:28 -0500

    decorator skill acquired

--- scraper.py
+++ scraper.py
@@ -1,5 +1,32 @@
-print('Data scraped successfully!')
-items = [1,2,3]
-print('Im going to delete this whole project')
-print('Sorry Michael')
-print('I forget how Python is written. Sorry Professor Bloomberg.')
+def greet(func):
+    def wrap(n):
+        print('Now you can decorate ... ')
+        func(n)
+        print('Anything in a decorator!')
+    return wrap
+
+@greet
+def hola(n):
+    print('yes' + n)
+
+user = "Michael"
+
+hola(user)
+
+
+'''
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_whee():
+    print("Whee!")
+
+say_whee()
+
+'''
